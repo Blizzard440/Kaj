f902aabffc8043dc2331fc84c27fbdf6778a2ba0
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2021-11-12 13:34:13 +0100

    .

--- uppg6.2.py
+++ uppg6.2.py
@@ -1,2 +1,14 @@
 import random
 
+tal = input('Skriv talen du vill räkna på:')
+ordlista = tal.split ()
+talunder0 = 0
+talöver0 = 0
+
+for t in ordlista:
+    if int(t) <= 0:
+        talunder0 = talunder0 + 1
+    else:
+        talöver0 = talöver0 + 1
+
+print(f'{talunder0} st tal under eller lika med 0 och {talöver0} st tal över 0')
