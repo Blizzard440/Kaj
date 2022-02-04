e1f3beb157ec53431eccc028cd8e61f5a4ff3f27
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2022-02-04 11:23:53 +0100

    .

--- slutuppgift/Slutuppgift.py
+++ slutuppgift/Slutuppgift.py
@@ -1,5 +1,9 @@
 import random
 
+lista = []
+mainstats = []
+substats = []
+
 mräknare1 = 0
 mräknare2 = 0
 eräknare1 = 0
@@ -9,6 +13,9 @@ eräknare4 = 0
 eräknare5 = 0
 
 
+mainstatökning = 15
+
+
 randomstatflat = [17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 randomstatökning = [5,6,7,8,9,10]
 randomstatöcrtcha = [6,7,8,9]
@@ -16,10 +23,67 @@ randomstatöcrtdmg = [7,8,9,10,11,12,13,14,15]
 randomstatöspd = [9,10,11,12,13,14]
 
 
-
-print('Du kommer nu att få en engravement kristall med random stats')
-
 stats = ['ATK%', 'ATK+', 'VIT%', 'VIT+', 'MDEF%', 'MDEF+', 'PDEF%', 'PDEF+', 'CRTCHA%', 'CRTDMG%', 'SPD+']
-randomstat = random.choice(stats)
+#randomstat = random.choice(stats)
+
+
+#print('Du kommer nu att få en engravement kristall med random stats')
 #print(stats[1])
-print(randomstat)
+#print(randomstat)
+
+
+for i in range(0,2):
+    randomstat = random.choice(stats)
+    if randomstat == stats[0]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    elif randomstat == stats[1]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    elif randomstat == stats[2]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    elif randomstat == stats[3]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    elif randomstat == stats[4]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    elif randomstat == stats[5]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    elif randomstat == stats[6]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    elif randomstat == stats[7]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    elif randomstat == stats[8]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    elif randomstat == stats[9]:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    else:
+        mainstats.append(randomstat)
+        mainstats.append(mainstatökning)
+
+    
+
+#for i in range(0,5):
+#    randomstat = random.choice(stats)
+#    substats.append(randomstat)
+
+
+print(mainstats)
+#print(substats)
