9bd6b7445765e00a2563264b77a6125b4a9d7176
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2022-02-24 20:22:26 +0100

    är denna godkänd? lite problem med att starta om programmet och få det att funka men annars så funkar de bra.

--- slutuppgift/Slutuppgift.py
+++ slutuppgift/Slutuppgift.py
@@ -6,20 +6,22 @@ mainstats = []
 substats = []
 allstats = []
 
-mräknare1 = 0
-mräknare2 = 0
-eräknare1 = 0
-eräknare2 = 0
-eräknare3 = 0
-eräknare4 = 0
-eräknare5 = 0
+#mräknare1 = 0
+#mräknare2 = 0
+#eräknare1 = 0
+#eräknare2 = 0
+#eräknare3 = 0
+#eräknare4 = 0
+#eräknare5 = 0
 
-fortsätt = []
+#fortsätt = []
 
 count = 0
 countsub = 0
+statscount = 0
 
 mainstatökning = 15
+mainstatsmax = 135
 mainstatökningflat = 50
 substatökning = 8
 
@@ -59,20 +61,30 @@ stats = ['ATK%', 'ATK+', 'VIT%', 'VIT+', 'MDEF%', 'MDEF+', 'PDEF%', 'PDEF+', 'CR
 inp1 = '0'
 
 while inp1 != '2':
+    # del mainstats[:]
+    # del substats[:]
+    # inp1 = '0'       FÖRSÖK att få programmet att ta bort alla inställningar som finns för att kunna slumpa nytt igen.
+    # count = 0
+    # countsub = 0
     print('')
     print('1 för att generera engravement kristall')
     print('2 för att stänga programmet')
     print('')
+    # print(count)
+    # print(countsub)
+    # print(mainstats)
+    # print(substats)
+
     inp1 = input('Vad vill du göra: ')
     
     if inp1 == '1':
 
         while count <= 1:
-            randomradnomstatflat = random.choice(randomstatflat)
-            randomstatprocent = random.choice(randomstatökning)
-            randomstatcrtcha = random.choice(randomstatöcrtcha)
-            randomstatcrtdmg = random.choice(randomstatöcrtdmg)
-            randomstatspd = random.choice(randomstatöspd)
+            # randomradnomstatflat = random.choice(randomstatflat)
+            # randomstatprocent = random.choice(randomstatökning)
+            # randomstatcrtcha = random.choice(randomstatöcrtcha)
+            # randomstatcrtdmg = random.choice(randomstatöcrtdmg)
+            # randomstatspd = random.choice(randomstatöspd)
             randomstat = random.choice(stats)
             if randomstat not in mainstats:
                 mainstats.append(randomstat)
@@ -83,11 +95,11 @@ while inp1 != '2':
 
 
         while countsub <= 4:
-            randomradnomstatflat = random.choice(randomstatflat)
-            randomstatprocent = random.choice(randomstatökning)
-            randomstatcrtcha = random.choice(randomstatöcrtcha)
-            randomstatcrtdmg = random.choice(randomstatöcrtdmg)
-            randomstatspd = random.choice(randomstatöspd)
+            # randomradnomstatflat = random.choice(randomstatflat)
+            # randomstatprocent = random.choice(randomstatökning)
+            # randomstatcrtcha = random.choice(randomstatöcrtcha)
+            # randomstatcrtdmg = random.choice(randomstatöcrtdmg)
+            # randomstatspd = random.choice(randomstatöspd)
             randomstat = random.choice(stats)
             if randomstat not in allstats:
                 substats.append(randomstat)
@@ -96,7 +108,8 @@ while inp1 != '2':
                 allstats.append(mainstatökning)
                 countsub = countsub + 1
 
-
+        
+        
         print('Engravement crystal current stats')
         print(mainstats)
         print(substats)
@@ -106,16 +119,40 @@ while inp1 != '2':
         print('Type "2" if upgrade to max level')
         print('Type "3" if you wish restart the program')
         inp = '0'
-        inp = input('What to do with the crystal? ')
+#        inp = input('What to do with the crystal? ')
 
         while inp != '3':
-    
+            inp = input('What to do with the crystal? ')
             if inp == '1':
+                print('')
                 print('You have successfully gotten rid of your crystal :D')
+                break
 
             elif inp == '2':
+                statscount = 0
+                print('')
                 print('Maxed stats:')
-                print('Här ska uppgraderade stats va')
+                while statscount < 9:
+                    mainstats[1] = mainstats[1] + mainstatökning
+                    mainstats[3] = mainstats[3] + mainstatökning
+
+                    substats[1] = substats[1] + randomstatprocent
+                    substats[3] = substats[3] + randomstatprocent
+                    substats[5] = substats[5] + randomstatprocent
+                    substats[7] = substats[7] + randomstatprocent
+                    substats[9] = substats[9] + randomstatprocent
+
+                    statscount = statscount + 1
+
+                print(mainstats)
+                print(substats)
+                print('')
+
+                print('Type 1 to restart program')
+                donext = input('Vad vill du göra? ')
+                if donext == '1':
+                    break
+
 
             elif inp == '3':
                 break
